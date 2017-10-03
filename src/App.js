82c7c55325abfd482f882a173e4fcf9d02e12e8d
Brett Reinhard <brett@reinhards.us>
2017-10-03 11:11:56 -0700

    Setting up webpage branch for contributions

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as API from './githubapi';
+
+class App extends Component {
+    componentDidMount() {
+        console.log(API.GET_RECURSIVE_TREE_PATH);
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+            </div>
+        );
+    }
+}
+
+export default App;
