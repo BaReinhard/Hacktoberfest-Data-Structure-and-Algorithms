07a0c01381bd76e055bff18e55a6ea700f1e8d49
Alexis LE CONTE <32488985+AlexisLeConte@users.noreply.github.com>
2017-10-03 20:59:00 +0200

    Update cstack.cpp

--- data_structures/stack/char_stack/cpp/cstack/cstack.cpp
+++ data_structures/stack/char_stack/cpp/cstack/cstack.cpp
@@ -1,14 +1,13 @@
 // Implementation File
 
-#include <iostream>
 #include "cstack.h"
 using namespace std;
 
 // Default Constructor to allow for no params being passed
 CStack::CStack()
 {
-    // Init top to be -1, array is empty
-    _top = -1;
+    // Init top to be nullptr, stack is empty
+    _top = nullptr;
 };
 // Removes value from the top of the stack
 void CStack::Pop()
@@ -16,48 +15,35 @@ void CStack::Pop()
     // Check if the array isnt empty, then pop off top stack value
     if (!IsEmpty())
     {
-        _top = _top - 1;
+        CElement *new_top = _top->_element_below;
+        delete _top;        //Do not forget to free memory
+        _top = new_top;
     }
 }
 // Pushes new value to the top of the stack
 void CStack::Push(char val)
 {
-    // Check if the array isn't full, then add new val
-    if (!IsFull())
-    {
-        // Increase _top to the next index
-        _top = _top + 1;
-        // Set new value to array location
-        _data[_top] = val;
-    }
+    CElement *new_top = new CElement();     //Allocate memory for the new top element
+    new_top->_element_below = _top;
+    new_top->_val = val;
+    _top = new_top;
 }
 // Returns value at top of the stack
 char CStack::Top()
 {
-
-    char empty;
-    // If array is empty, return empty character
+    // If array is empty, return null character
     if (IsEmpty())
     {
-        return empty;
+        return '\0';
         // Else return top Stack value
     }
     else
     {
-        return _data[_top];
+        return _top->_val;
     }
 }
 // Checks whether the stack is empty, returns bool
 bool CStack::IsEmpty()
 {
-    // Check if the index is -1, if so return true.
-    // If not return false
-    return _top == -1;
-}
-// Checks whether the Stack is full, returns bool
-bool CStack::IsFull()
-{
-    // Check if the index is less than equal to 18, if so return true.
-    // If not return false
-    return _top >= 18;
+    return _top == nullptr;
 }
