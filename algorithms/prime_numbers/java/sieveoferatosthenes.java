ebe4671c5c716eb9a47625d1324e59bba501a816
darkLord19 <darklord.u999@gmail.com>
2017-10-04 12:00:25 +0530

    Added Algorithm Sieve of Eratosthenes

---
+++ algorithms/prime_numbers/java/sieveoferatosthenes.java
@@ -0,0 +1,26 @@
+// Java program to print all primes smaller than or equal to
+// n using Sieve of Eratosthenes
+ 
+class SieveOfEratosthenes {
+    
+	void sieveOfEratosthenes(int n){
+        
+		boolean prime[] = new boolean[n+1];
+        	for(int i=0;i<n;i++)
+        		prime[i] = true;
+         
+        	for(int p = 2; p*p <=n; p++) {
+
+			if(prime[p] == true){
+	                	for(int i = p*2; i <= n; i += p)
+        		        	prime[i] = false;
+            		}
+        	}
+         
+        	for(int i = 2; i <= n; i++) {
+            		if(prime[i] == true)
+               			System.out.print(i + " ");
+        	}
+    	}
+
+}
