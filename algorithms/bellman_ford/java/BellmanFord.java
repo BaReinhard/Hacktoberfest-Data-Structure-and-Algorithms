f8919e7ccd83f786e5d228f4179e90d1273ed17b
Daniel Toniuc <dtoniuc@gmail.com>
2017-10-16 21:07:51 +0300

    Fix bellmanford corner cases
    Refactored the code to allo for an easier
    initialization of the graph and better information
    hiding. Treated the cases of invalid starting
    node, null graph or empty graph. Fixed the message
    displayed in case of graph with negative cycle.
    
    Fixes: https://github.com/BaReinhard/Hacktoberfest-Data-Structure-and-Algorithms/issues/235

--- algorithms/bellman_ford/java/BellmanFord.java
+++ algorithms/bellman_ford/java/BellmanFord.java
@@ -1,107 +1,95 @@
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
 class Graph
 {
-	class Edge {
+	private class Edge {
 		int src, dest, weight;
-		Edge() {
-			src = dest = weight = 0;
+		private Edge(int src, int dest, int weight) {
+			this.src = src;
+			this.dest = dest;
+			this.weight = weight;
 		}
 	};
 
-	int V, E;
-	Edge edge[];
-	Graph(int v, int e)
+	private int V;
+	private LinkedList<Edge> edgeList;
+	public Graph(int v)
 	{
-		V = v;
-		E = e;
-		edge = new Edge[e];
-		for (int i=0; i<e; ++i)
-			edge[i] = new Edge();
+		this.V = v;
+		edgeList = new LinkedList<Edge>();
 	}
 
-	void BellmanFord(Graph graph,int src)
-	{
-		int V = graph.V, E = graph.E;
+	public void bellmanFord(int src) {
+		if (src < 0 || src > (V - 1)) {
+			System.out.println("Invalid starting vertex");
+			return;
+		}
+		
 		int dist[] = new int[V];
 
 		for (int i=0; i<V; ++i)
 			dist[i] = Integer.MAX_VALUE;
 		dist[src] = 0;
 
-		for (int i=1; i<V; ++i)
-		{
-			for (int j=0; j<E; ++j)
-			{
-				int u = graph.edge[j].src;
-				int v = graph.edge[j].dest;
-				int weight = graph.edge[j].weight;
-				if (dist[u]!=Integer.MAX_VALUE &&
-					dist[u]+weight<dist[v])
+		for (int i=1; i < V; ++i) {
+			for (int j=0; j <  edgeList.size(); ++j) {		
+				int u = edgeList.get(j).src;
+				int v = edgeList.get(j).dest;
+				int weight = edgeList.get(j).weight;
+				
+				if (dist[u] != Integer.MAX_VALUE && dist[u] + weight<dist[v]) {
 					dist[v]=dist[u]+weight;
+				}
 			}
 		}
 
-		for (int j=0; j<E; ++j)
-		{
-			int u = graph.edge[j].src;
-			int v = graph.edge[j].dest;
-			int weight = graph.edge[j].weight;
-			if (dist[u]!=Integer.MAX_VALUE &&
-				dist[u]+weight<dist[v])
-			System.out.println("Graph contains negative weight cycle");
+		for (int j = 0; j < edgeList.size(); ++j) {
+			int u = edgeList.get(j).src;
+			int v = edgeList.get(j).dest;
+			int weight = edgeList.get(j).weight;
+			
+			if (dist[u] != Integer.MAX_VALUE && dist[u] + weight < dist[v]) {
+				System.out.println("Graph contains negative weight cycle");
+				return;
+			}
+			
 		}
+		
 		printArr(dist, V);
 	}
 
-	void printArr(int dist[], int V)
+	private void printArr(int dist[], int V)
 	{
 		System.out.println("Vertex Distance from Source");
 		for (int i=0; i<V; ++i)
 			System.out.println(i+"tt"+dist[i]);
 	}
+	
+	public boolean addEdge(int src, int dest, int weight) {
+		if (src < 0 || dest < 0 || src > (V - 1) || dest > (V - 1)) {
+			return false;
+		}
+		
+		Edge edge = new Edge(src, dest, weight);
+		edgeList.add(edge);
+		return true;
+	}
 
 	public static void main(String[] args)
 	{
 		int V = 5;
-		int E = 8;
 
-		Graph graph = new Graph(V, E);
+		Graph graph = new Graph(V);
+		
+		graph.addEdge(0, 1, -1);
+		graph.addEdge(0, 2, 4);
+		graph.addEdge(1, 2, 3);
+		graph.addEdge(1, 3, 2);
+		graph.addEdge(1, 4, 2);
+		graph.addEdge(3, 2, 5);
+		graph.addEdge(3, 1, 1);
+		graph.addEdge(4, 3, -3);
 
-		graph.edge[0].src = 0;
-		graph.edge[0].dest = 1;
-		graph.edge[0].weight = -1;
-
-		graph.edge[1].src = 0;
-		graph.edge[1].dest = 2;
-		graph.edge[1].weight = 4;
-
-		graph.edge[2].src = 1;
-		graph.edge[2].dest = 2;
-		graph.edge[2].weight = 3;
-
-		graph.edge[3].src = 1;
-		graph.edge[3].dest = 3;
-		graph.edge[3].weight = 2;
-
-		graph.edge[4].src = 1;
-		graph.edge[4].dest = 4;
-		graph.edge[4].weight = 2;
-
-		graph.edge[5].src = 3;
-		graph.edge[5].dest = 2;
-		graph.edge[5].weight = 5;
-
-		graph.edge[6].src = 3;
-		graph.edge[6].dest = 1;
-		graph.edge[6].weight = 1;
-
-		graph.edge[7].src = 4;
-		graph.edge[7].dest = 3;
-		graph.edge[7].weight = -3;
-
-		graph.BellmanFord(graph, 0);
+		graph.bellmanFord(1);
 	}
 }
