38595b428149a650189f362d17e663a1e47e445c
Mohamed Elgaar <mohamed@kaist.ac.kr>
2017-10-19 19:59:04 +0900

    Added Euclidean Algorithm in java

---
+++ algorithms/EuclideanAlgorithm/java/EuclideanAlgorithm.java
@@ -0,0 +1,13 @@
+public class EuclideanAlgorithm{
+
+	public static void main(String[] args){
+		System.out.println(gcd(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
+	}
+
+	public static int gcd(int a, int b){
+		if(b==0){
+			return a;
+		}
+		return gcd(b,a%b);
+	}
+}
