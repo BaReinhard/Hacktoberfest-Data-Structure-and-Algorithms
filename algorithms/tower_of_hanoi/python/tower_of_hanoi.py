b97b65ebd91972ad09038e0112379047f747b125
narimiran <miran.tuhtan+git@gmail.com>
2017-10-21 11:34:40 +0200

    tower of hanoi - correct python version

--- algorithms/tower_of_hanoi/python/tower_of_hanoi.py
+++ algorithms/tower_of_hanoi/python/tower_of_hanoi.py
@@ -1,18 +1,18 @@
-
-def move_disk(from_pole, to_pole):
+def move_disk(height, from_pole, to_pole):
     '''
     Prints the movement
     '''
-    print('Moving disk from', from_pole, ' to ', to_pole)
+    print('Moving disk {} from {} to {}'.format(height, from_pole, to_pole))
 
 def tower_of_hanoi(height, from_pole=1, to_pole=2, aux=3):
     '''
     Tower of Hanoi implementation
     '''
     if height > 0:
-        tower_of_hanoi(height - 1, from_pole, to_pole, aux)
-        move_disk(from_pole, to_pole)
-        tower_of_hanoi(height - 1, aux, from_pole, to_pole)
+        tower_of_hanoi(height - 1, from_pole, aux, to_pole)
+        move_disk(height, from_pole, to_pole)
+        tower_of_hanoi(height - 1, aux, to_pole, from_pole)
+
 
 if __name__ == '__main__':
     tower_of_hanoi(3)
