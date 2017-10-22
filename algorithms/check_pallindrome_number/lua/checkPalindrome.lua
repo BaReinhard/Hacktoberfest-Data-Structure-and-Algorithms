0b10ea5a2fee78b1f4f331aa904ef751c1843ec9
Nicholas DeLello <nicholas_delello@yahoo.com>
2017-10-22 19:47:06 -0400

    Remove palindrome.

--- algorithms/check_pallindrome_number/lua/checkPalindrome.lua
+++
@@ -1,5 +0,0 @@
-local function isPalindrome(s)
-    s = tostring(s)
-    return s:sub(1, math.floor(#s / 2 + .5)) == s:sub(math.ceil(#s / 2 + .5)):reverse()
-end
-print(isPalindrome(table.concat({ ... }, "")))
