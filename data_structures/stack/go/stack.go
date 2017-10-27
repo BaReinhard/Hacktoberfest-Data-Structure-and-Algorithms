79c98b8066d8a8788724d30e3539617cd3ee4968
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2017-10-27 15:28:20 +0100

    Update stack.go
    Removed unnecessary characters

--- data_structures/stack/go/stack.go
+++ data_structures/stack/go/stack.go
@@ -12,7 +12,7 @@ func (s *stack) Pop() interface{} {
 	//get last item
 	value := s.data[len(s.data)-1]
 	//shrink array
-	s.data = s.data[0 : len(s.data)-1]
+	s.data = s.data[:len(s.data)-1]
 
 	return value
 }
