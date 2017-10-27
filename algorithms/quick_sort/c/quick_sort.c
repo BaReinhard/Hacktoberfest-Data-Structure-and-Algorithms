64f2fa476ec1c7ebe439a4ddeda3bde3d04b4700
Francisco Nievas <frannievas@gmail.com>
2017-10-26 23:32:29 -0300

    Added recursion version of quick sort

--- algorithms/quick_sort/c/quick_sort.c
+++ algorithms/quick_sort/c/quick_sort.c
@@ -1,4 +1,41 @@
 #include<stdio.h>
+
+bool array_is_valid(int *a, unsigned int length) {
+    return ((length == 0 && a == NULL) || (length != 0 && a != NULL));
+}
+
+bool array_is_sorted(int *a, unsigned int length) {
+  bool sorted = true;
+  unsigned int i = 0;
+  while( (i+1) < length && sorted){
+    if(a[i] > a[i+1]){
+      sorted = false;
+      break;
+    }
+    i++;
+  }
+  return sorted;
+}
+
+void quick_sort_rec(int *a, int izq, int der){
+    int piv;
+
+    if ( der > izq) {
+      piv = pivot(a,izq,der);
+      quick_sort_rec(a,izq,piv-1);
+      quick_sort_rec(a,piv+1,der);
+    }
+    return;
+}
+
+
+void quick_sort(int *a, unsigned int length) {
+
+    quick_sort_rec(a,0,length-1);
+    /* Check postconditions */
+    assert(array_is_sorted(a, length));
+}
+
 void quicksort(int number[25],int first,int last){
    int i, j, pivot, temp;
 
