ee0a1a5fc6008064f29874e996ee1fb672136a6d
Ravi Teja Gannavarapu <IamRaviTejaG@users.noreply.github.com>
2017-10-29 22:09:11 +0530

    Update CaesarCipher.py

--- algorithms/caesar_cipher/python/CaesarCipher.py
+++ algorithms/caesar_cipher/python/CaesarCipher.py
@@ -2,10 +2,10 @@ cipher = []
 a = raw_input("Input string: ")
 shift = int(raw_input("Shift: "))
 for i in a:
-	j = ord(i)
-	if (i.isupper()):
-		cipher.append(chr((j+shift-65)%26 + 65))
-	else:
-		cipher.append(chr((j+shift-97)%26 + 97))
-	b = ''.join(cipher)
+    j = ord(i)
+    if (i.isupper()):
+        cipher.append(chr((j+shift-65)%26 + 65))
+    else:
+        cipher.append(chr((j+shift-97)%26 + 97))
+    b = ''.join(cipher)
 print ("Cipher: " + b)
