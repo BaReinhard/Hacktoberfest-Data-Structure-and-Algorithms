59f3ae05ada599478d6fbfe2762e2e6b84b2d344
Saish Mhatre <lambocrypt@protonmail.com>
2018-10-01 21:25:45 +0530

    Added More Comments For Better Understanding
    Added More Comments For Better Understanding The Algorithm Of Stack

--- data_structures/stack/c/stack.c
+++ data_structures/stack/c/stack.c
@@ -11,37 +11,40 @@ int top=-1;
 
 void main()
 {
-int value,choice;
-while(1)
-{
-printf("***************************\n");
-printf("1.push\n2.pop\n3.display\n");
-printf("Enter your choice\n");
-scanf("%d",&choice);
-
-switch(choice)
-{
-case 1:printf("enter the value to be push into stack\n ");
-scanf("%d",&value);	
-push(value);
-break ;
-case 2:pop();
- break;
-case 3:display();
- break;
-default:printf("wrong input please try again\n");
-  }//end of switch
+	//valaue variable used to store elements in stack
+	//choice variable is used to decide what function to use
+	int value,choice;
+	
+	while(1)
+ {
+	printf("***************************\n");
+	printf("1.push\n2.pop\n3.display\n");
+	printf("Enter your choice\n");
+	scanf("%d",&choice);
+	//Here We Are Using Switch Case For Push , Pop & Display Function
+		switch(choice)
+	 {
+		case 1:printf("enter the value to be push into stack\n ");
+		scanf("%d",&value);	
+		push(value);
+		break ;
+		case 2:pop();
+		 break;
+		case 3:display();
+		 break;
+		default:printf("wrong input please try again\n");
+	  }//end of switch
  }//end of while
 }//end of main
 
 void display(){
 	if(top==-1)
 	{	
-	  printf("Stack is empty\n\n");	
+	  	printf("Stack is empty\n\n");	
 	}
 	else
 	{ 		 
- 	 printf("\nstack contains %d elements\n",top+1); 	
+ 		 printf("\nstack contains %d elements\n",top+1); 	
 	}
 	int i=0;
 	for(i=top;i>=0;i--)
@@ -58,8 +61,10 @@ void push(int value)
 	}
 	else
 	{
-	     top=top+1;
-	stack[top]=value;
+		//this statement will add wlwmwnts in stack
+	     	top=top+1;
+		//this statement will declared latest value which was inputed in stack as Top
+		stack[top]=value;
 	}
 }
 
@@ -72,7 +77,9 @@ void pop()
 	}
 	else
 	{
-	top=top-1;	
+		//This will pop elements from stack
+		top=top-1;
+		
 	}
 }
 
