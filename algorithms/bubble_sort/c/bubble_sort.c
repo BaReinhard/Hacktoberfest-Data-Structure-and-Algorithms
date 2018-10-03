17d4f35115d37f922f6788164edf96a608e1b348
Gouri S Nair <39928477+gourisnair@users.noreply.github.com>
2018-10-03 22:33:25 +0530

    Update bubble_sort.c

--- algorithms/bubble_sort/c/bubble_sort.c
+++ algorithms/bubble_sort/c/bubble_sort.c
@@ -8,26 +8,27 @@
 
 
 
-void bubbleSort(int arr[], int n)
-{
-   int i, j;
-   for (i = 0; i < n-1; i++)      
- 
-       // Last i elements are already in place   
-       for (j = 0; j < n-i-1; j++) 
-           if (arr[j] > arr[j+1])
-              swap(&arr[j], &arr[j+1]);
-}
-
+#include<stdio.h>
 int main()
 {
-int i,j,k;
-int a[5];
-for(i=0;i<5;i++)
-scanf("%d",&a[i]);
-bubblesort(a,5);
-for(i=0;i<5;i++)
-printf("%d\n",a[i]);
+  int i, j, temp, size;
+  printf("Enter size of array: ");
+  scanf("%d", &size);
+  int a[size];
+  for(i = 0; i < size; i++) {
+    scanf("%d", &a[i]);
+  }
+  for(i = 0; i < size; i++) {
+    for(j = 1; j < size - i; j++) {
+      if(a[i] < a[j]) {
+        temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+  }
 
-return 0;
+  for(i = 0; i < size; i++) {
+    printf("%d", a[i]);
+  }
 }
