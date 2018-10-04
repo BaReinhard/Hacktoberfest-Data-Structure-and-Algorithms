99c5aafef9caea58d37ad539a425b2d49290f8d7
gunawardanadulaj935 <gunawardanadulaj935@gmail.com>
2018-10-05 03:47:33 +0530

    add gcd in java

---
+++ data_structures/gcd/gcd.java
@@ -0,0 +1,16 @@
+public class GCD {
+
+    public static void main(String[] args) {
+
+        int n1 = 81, n2 = 153, gcd = 1;
+
+        for(int i = 1; i <= n1 && i <= n2; ++i)
+        {
+            // Checks if i is factor of both integers
+            if(n1 % i==0 && n2 % i==0)
+                gcd = i;
+        }
+
+        System.out.printf("G.C.D of %d and %d is %d", n1, n2, gcd);
+    }
+}
