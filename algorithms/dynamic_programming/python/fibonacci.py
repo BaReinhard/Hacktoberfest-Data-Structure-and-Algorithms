ae3ae4b9965bd4584693ffbca7ee3f45fa7a6246
thesourabh <sourabhshetty@gmail.com>
2018-10-29 00:59:55 -0400

    Added fibonacci.py (dynamic programming) in python

---
+++ algorithms/dynamic_programming/python/fibonacci.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+  if n <= 1:
+    return n
+  memo = [0] * (n+1)
+  memo[0], memo[1] = 0, 1
+  for i in range(2, n+1):
+    memo[i] = memo[i-1] + memo[i-2]
+  print(memo[n])
+  return memo[n]
+  
+  
+fibonacci(12)
