3796c8d52ffc8d2a9c5e236b8c177bc9135ce840
sanidhayarora <sanidhayarora@gmail.com>
2018-10-31 22:06:48 +0530

    kruskal_algorithm added

---
+++ algorithms/kruskal_algorithm/kruskal.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef long long int ll;
+typedef struct Node{
+    ll weight;
+    int node1;
+    int node2;
+}Node;
+
+#define MAX 100005;
+
+int id[10005], nodes, edges;
+Node p[10005];
+
+void initialize()
+{
+    for(int i = 0;i < MAX;++i)
+        id[i] = i;
+}
+
+int root(int x)
+{
+    while(id[x] != x)
+    {
+        id[x] = id[id[x]];
+        x = id[x];
+    }
+    return x;
+}
+
+void union_1(int x, int y)
+{
+    int p = root(x);
+    int q = root(y);
+    id[p] = id[q];
+}
+
+ll kruskal(Node p[])
+{
+    int x, y;
+    ll cost, minimumCost = 0;
+    for(int i = 0;i < edges;++i)
+    {
+        cost = p[i].weight;
+        x = p[i].node1;
+        y = p[i].node2;
+        if(root(y) != root(x))
+        {
+            minimumCost += cost;
+            union_1(x, y);
+        }    
+    }
+    return minimumCost;
+}
+
+int comp(const void *p, const void *q)
+{
+    Node* x = (Node*)p;
+    Node* y = (Node*)q;
+    if(x->weight < y->weight)
+        return 1;
+    else if(x->weight == y->weight)
+        if(x->node1 > y->node1)
+            return 1;
+    return -1;
+}
+
+int main()
+{
+    int x, y;
+    ll weight, cost, minCost;
+    initialize();
+    scanf("%d%d",&nodes,&edges);
+    for(int i = 0;i < edges;++i)
+    {
+        scanf("%d%d%lld",&x,&y,&weight);
+        p[i].weight = weight;
+        p[i].node1 = x;
+        p[i].node2 = y;
+    }
+    qsort(p, edges, sizeof(Node), comp);
+    minCost = kruskal(p);
+    printf("%lld\n", minCost);
+    return 0;
+}
