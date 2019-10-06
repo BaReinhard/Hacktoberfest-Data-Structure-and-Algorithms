07829abe905da912933847f33e57bda91362da0f
mayank18049 <mayank18049@iiitd.ac.in>
2019-10-06 07:16:07 -0700

    added closest algorithm in python3

--- algorithms/closest/python/closest.py
+++ algorithms/closest/python/closest.py
@@ -2,7 +2,7 @@ inp_arr = list(map(int,input().split()))
 inp_arr.sort()
 closest = float('inf')
 for i in range(1,len(inp_arr)):
-	temp = abs(inp[i]-inp[i-1])
+	temp = abs(inp_arr[i]-inp_arr[i-1])
 	closest = min(temp,closest)
 
 print(closest)
