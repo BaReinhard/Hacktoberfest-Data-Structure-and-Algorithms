096e495269148afac5c59b4f85afc6be5755077b
Viktor <31904328+EpmakJS@users.noreply.github.com>
2019-10-08 17:34:37 +0300

    Update heap_sort.js

--- algorithms/heap_sort/javascript/heap_sort.js
+++ algorithms/heap_sort/javascript/heap_sort.js
@@ -1,48 +1,73 @@
-  var array_length;
-/* to create MAX  array */  
-function heap_root(input, i) {
-    var left = 2 * i + 1;
-    var right = 2 * i + 2;
-    var max = i;
-
-    if (left < array_length && input[left] > input[max]) {
-        max = left;
-    }
-
-    if (right < array_length && input[right] > input[max])     {
-        max = right;
-    }
-
-    if (max != i) {
-        swap(input, i, max);
-        heap_root(input, max);
-    }
-}
-
-function swap(input, index_A, index_B) {
-    var temp = input[index_A];
-
-    input[index_A] = input[index_B];
-    input[index_B] = temp;
-}
-
-function heapSort(input) {
-    
-    array_length = input.length;
-
-    for (var i = Math.floor(array_length / 2); i >= 0; i -= 1)      {
-        heap_root(input, i);
-      }
-
-    for (i = input.length - 1; i > 0; i--) {
-        swap(input, 0, i);
-        array_length--;
-      
-      
-        heap_root(input, 0);
-    }
-}
-
-var arr = [3, 0, 2, 5, -1, 4, 1];
+/*
+ * Example
+ */
+arr = [1,4,3,-0,-9,12];
 heapSort(arr);
 console.log(arr);
+
+/*
+ * Swap array elements
+ */
+function swap(arr, firstItemIndex, lastItemIndex) {
+  var temp = arr[firstItemIndex];
+  arr[firstItemIndex] = arr[lastItemIndex];
+  arr[lastItemIndex] = temp;
+}
+
+/*
+ * Sort elements by comparing values
+ */
+function heapify(arr, i, max) {
+  var index, leftChild, rightChild;
+
+  while (i < max) {
+    index = i;
+    leftChild = 2 * i + 1;
+    rightChild = leftChild + 1;
+
+    if (leftChild < max && arr[leftChild] > arr[index]) {
+      index = leftChild;
+    }
+
+    if (rightChild < max && arr[rightChild] > arr[index]) {
+      index = rightChild;
+    }
+
+    if (index === i) {
+      return;
+    }
+
+    swap(arr, i, index);
+
+    i = index;
+  }
+}
+
+/*
+ * convert list in to max heap 
+ */
+function buildMaxHeap(arr) {
+  var i;
+  i = arr.length / 2 - 1;
+  i = Math.floor(i);
+
+  while (i >= 0) {
+    heapify(arr, i, arr.length);
+    i -= 1;
+  }
+}
+
+
+/*
+ * Steps for heap sort
+ */
+function heapSort(arr) {
+  buildMaxHeap(arr);
+  lastElement = arr.length - 1;
+
+  while (lastElement > 0) {
+    swap(arr, 0, lastElement);
+    heapify(arr, 0, lastElement);
+    lastElement -= 1;
+  }
+}
