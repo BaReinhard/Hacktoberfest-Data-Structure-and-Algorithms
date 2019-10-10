5af173dc23454379e5ee9652a3c277695feb4bc2
Vinayakjaiswal07 <vinayakjaiswal48@gmail.com>
2019-10-10 16:49:34 +0530

    adding level order code

---
+++ data_structures/binary_tree/python/binary_tree_level_order.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def levelOrder(root):
+    if root is None:
+        return 0
+    q=[]
+    q.append(root)
+    while(len(q)>0):
+        print(q[0],end=" ")
+        node=q.pop(0)
+        if node.left is not None:
+            q.append(node.left)
+        if node.right is not None:
+            q.append(node.right)
+
