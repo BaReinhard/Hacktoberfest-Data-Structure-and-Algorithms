162e8d34a03da360f261e3ab94e82cabec97c070
bogdan.terec <bogdan.terec@fxbits.io>
2019-10-12 18:29:29 +0300

    Add string array

---
+++ data_structures/array/java/array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class arraylist {
+
+   public static void main(String args[]) {
+      // create an array
+      String array[] = new String[]; {
+              "Toyota", "Mercedes", "BMW", "Volkswagen", "Skoda" };
+
+      System.out.println("Size of the array: " + array.length);
+
+      // display the array list
+      System.out.println("Contents of the array: " + array);
+
+   }
+}
