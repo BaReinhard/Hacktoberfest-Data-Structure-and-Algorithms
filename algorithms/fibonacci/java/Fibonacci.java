4ea83c360a8f233b0a03f1e8a0e63df7214bf97e
crouther <33792608+crouther@users.noreply.github.com>
2019-10-18 16:25:35 -0400

    Update Fibonacci.java

--- algorithms/fibonacci/java/Fibonacci.java
+++ algorithms/fibonacci/java/Fibonacci.java
@@ -1,11 +1,11 @@
 public class Fibonacci{
 	public static void main(String args[]){
-		System.out.println("Test Value 10 = " + nth(10));
+		System.out.println("The nth term where n = 10: " + nth(10));
 	}
 
-	public static int nth(int i){
+	public static int nth(int n){
 		return ( (int) ( (double) 
 			( 1 / (double) Math.sqrt(5) ) * 
-			Math.pow( ( (1 + Math.sqrt(5)) / 2 ), (i + 1))) );
+			Math.pow( ( (1 + Math.sqrt(5)) / 2 ), (n + 1))) );
 	}
 }
