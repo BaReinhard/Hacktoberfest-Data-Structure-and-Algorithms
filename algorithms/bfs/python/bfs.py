f76eb7135770b23ab844b1254cea0bba6b4d1fdc
yenhoangle <yhle@ucsd.edu>
2019-10-21 22:34:04 -0700

    Added breath first search implementation in python

---
+++ algorithms/bfs/python/bfs.py
@@ -0,0 +1,16 @@
+import collections
+
+
+def bfs(graph, root): 
+    visited, queue = set(), collections.deque([root])
+    while queue: 
+        vertex = queue.popleft()
+        for neighbour in graph[vertex]: 
+            if neighbour not in visited: 
+                visited.add(neighbour) 
+                queue.append(neighbour) 
+
+
+if __name__ == '__main__':
+    graph = {0: [1, 2], 1: [2], 2: []} 
+    breadth_first_search(graph, 0)
