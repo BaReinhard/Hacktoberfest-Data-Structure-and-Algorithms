28de3a04bc94fa639c7752ed69562eed78c18f33
sangeetagupta <tinnigupta1998@gmail.com>
2019-10-26 14:29:13 +0530

    Implementation of HashSet for user defined classes.

--- data_structures/hashset/java/ProductSetDemo.java
+++ data_structures/hashset/java/ProductSetDemo.java
@@ -7,8 +7,8 @@ public class ProductSetDemo{
         Product product3 = new Product(1,"Samsung Galaxy A series",80000f);
         Product product4 = new Product(1,"Samsung Galaxy A series",80000f);
 
-        //memory address se kar raha hai. Hence for product1 and product4, same values are being stored.
         HashSet<Product> products = new HashSet<>();
+
         //add() internally calls hashCode() and equals(),hence it is important that we override them so
         // that no two objects can exist with same values.
         products.add(product1);
