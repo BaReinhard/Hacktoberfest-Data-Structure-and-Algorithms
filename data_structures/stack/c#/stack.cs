e2f135dcc58de8cbcdde796d35a6d93f7f4a1352
Henrique Fernandes <henrique.fquick@gmail.com>
2020-09-24 18:04:10 -0300

    Update stack.cs
    Check modifications made to avoid exceptions

--- data_structures/stack/c#/stack.cs
+++ data_structures/stack/c#/stack.cs
@@ -3,27 +3,35 @@ using System.Collections.Generic;
  
 namespace Stack{
    public class Stack<t>  {
-        private List<t> _array;     // Storage for stack elements 
-        private int _size;       
+        private List<t> _array;     // Storage for stack elements  
         private int _top;    // Number of items in the stack.
         static List<t> _emptyArray = new List<t>(); 
+    
+       public int Size { get { return _array.Count; } }
+    
         public Stack(){
             
             _array = _emptyArray;
-            _size = 0; 
             _top = -1;
             
         }
         public void Push(t data){
             _array.Add(data);
-            _size++;
             _top++;
         }
         public void Pop(){
-            _array.RemoveAt(_top--);
+            if(_top != -1 )
+                _array.RemoveAt(_top--);
+            else
+                Console.WriteLine("There is no element on your stack.");
         }
         public t Top(){
-            return _array[_top];
+            if(_top != -1 ){
+                return _array[_top];
+            }else{
+                Console.WriteLine("There is no element on your stack.");
+                return default(t);
+            }
         }
     }
 }
