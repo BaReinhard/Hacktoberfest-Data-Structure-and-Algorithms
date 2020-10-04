25db85d7dd7342b6d5e2d44b6a10b9029b273e5d
Damjan-Actual <damjandimitrov1@gmail.com>
2020-10-04 15:01:26 +0200

    Remove comments

--- data_structures/skip_list/java/SkipList.java
+++ data_structures/skip_list/java/SkipList.java
@@ -13,21 +13,13 @@ public class SkipList
 		HEAD, TAIL
 	}
 
-	/*
-	 * Tvoritelj sprejme kot parameter stevilo elementov, ki jih je sposoben
-	 * obdelati
-	 */
+	
 	public SkipList(long maxNodes)
 	{
 		this.maxHeight = (int) Math.round(Math.log(maxNodes) / Math.log(2)); // Logarithm with base 2
 		this.head = new NodeSkipList(Integer.MIN_VALUE, maxHeight);
 	}
-
-	/*
-	 * Metoda sprejme stevilo in ga vstavi v preskocni seznam. Ce element ze obstaja
-	 * v podatkovni strukturi, vrne false. Metoda vrne true, ce je bil element
-	 * uspesno vstavljen in false sicer.
-	 */
+	
 	public boolean insert(int searchKey)
 	{
 		int level = getRandomLevel();
@@ -56,10 +48,6 @@ public class SkipList
 		return true;
 	}
 
-	/*
-	 * Metoda sprejme stevilo in poisce element v preskocnem seznamu. Metoda vrne
-	 * true, ce je bil element uspesno najden v podatkovni strukturi, in false sicer
-	 */
 	public boolean search(int searchKey)
 	{
 		NodeSkipList currentNode = head;
@@ -78,11 +66,6 @@ public class SkipList
 		return false;
 	}
 
-	/*
-	 * Metoda sprejme stevilo in izbrise element iz preskocnega seznama. Metoda vrne
-	 * true, ce je bil element uspesno izbrisan iz podatkovne strukture, in false
-	 * sicer
-	 */
 	public boolean delete(int key)
 	{
 		NodeSkipList currentNode = head, nextNode = null;
